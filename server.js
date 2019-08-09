edbcf9cb2ae42f8c42016cbf0e4b46f087f4b725
fxc0931 <2568681147@qq.com>
2019-08-09 15:42:03 +0800

    make it better

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require("express")
 const mongoose = require("mongoose")
 const app = express()
 
+
+//设置允许跨域访问该服务.
+app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    //Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
+    res.header('Access-Control-Allow-Methods', '*')
+    res.header('Content-Type', 'application/json;charset=utf-8')
+    next()
+})
+
+
 // 引入user.js
 const users = require("./routes/api/users")
 
